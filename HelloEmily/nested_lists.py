9889f1d3eb6d456d1f5af8269de461c5e78a2702
matsuyama2000 <emily20000222@keio.jp>
2022-09-03 01:36:42 +0900

    Working with Nested Lists

---
+++ HelloEmily/nested_lists.py
@@ -0,0 +1,5 @@
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+veggies = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, veggies]
+print(dirty_dozen)
