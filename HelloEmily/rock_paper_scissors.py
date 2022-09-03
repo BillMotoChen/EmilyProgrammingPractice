6bfd9c3fcf420c72e4bb03d48851e5776f4e7245
matsuyama2000 <emily20000222@keio.jp>
2022-09-03 18:34:05 +0900

    revised several lines

--- HelloEmily/rock_paper_scissors.py
+++ HelloEmily/rock_paper_scissors.py
@@ -27,23 +27,15 @@ scissors = '''
 
 import random
 
+game_images = [rock, paper, scissors]
+
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if user_choice == 0:
-  print(rock)
-elif user_choice == 1:
-  print(paper)
-elif user_choice == 2:
-  print(scissors)
+print(game_images[user_choice])
 
 # Generates a random number between 0 and 2
 computer_choice = random.randint(0, 2)
 print(f"Computer chose {computer_choice}")
-if computer_choice == 0:
-  print(rock)
-elif computer_choice == 1:
-  print(paper)
-elif computer_choice == 2:
-  print(scissors)
+print(game_images[computer_choice])
 
 # compare user_choice with computer_choice
 if user_choice >= 3 or user_choice < 0:
