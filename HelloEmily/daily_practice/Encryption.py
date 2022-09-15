f2a830b7eb2997a483c1870d239dd2b9b8d0b986
matsuyama2000 <emily20000222@keio.jp>
2022-09-15 21:50:01 +0900

    debug. What happens if you try to encode the word 'civilization'? duplicate the alphabet list to solve this issue.

--- HelloEmily/daily_practice/Encryption.py
+++ HelloEmily/daily_practice/Encryption.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -22,6 +22,7 @@ def encrypt(plain_text, shift_amount):
     print(f"The encoded text is {encode_text}")
 
     #🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+    # duplicate the alphabet list to solve this issue.
 
 #Call the encrypt function and pass in the user inputs. should be able to test the code and encrypt a message.
 encrypt(plain_text = text, shift_amount = shift)
