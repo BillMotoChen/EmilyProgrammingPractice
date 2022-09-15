928bf72cc615facfeaacee8a050e5c307b500f39
matsuyama2000 <emily20000222@keio.jp>
2022-09-16 02:02:30 +0900

    learn Python Dictionary

--- HelloEmily/daily_practice/learn_pytjoon_dic.py
+++ HelloEmily/daily_practice/learn_pytjoon_dic.py
@@ -1,7 +1,30 @@
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
+    123: "hello bill",
 }
 
+# retrieving items from the dictionary
 # dictionary[key]
 print(programming_dictionary["Bug"])
+print(programming_dictionary[123])
+
+# adding new items to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
+
+# create an empty dictionary
+empty_dictionary = {}
+
+# wipe an existing dictionary
+programming_dictionary = {}
+print(programming_dictionary)
+
+# edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+print(programming_dictionary)
+
+# loop through a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
