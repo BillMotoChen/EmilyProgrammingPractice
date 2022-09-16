545a0a1edc6031430bba772e20bf7dfcb17f08e2
matsuyama2000 <emily20000222@keio.jp>
2022-09-16 16:59:02 +0900

    grading program

---
+++ HelloEmily/daily_practice/grading_program.py
@@ -0,0 +1,23 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+# Create an empty dictionary called student_grades.
+student_grades = {}
+
+# add the grades to student_grades.
+for name in student_scores:
+    if student_scores[name] >= 91 and student_scores[name] <= 100:
+        student_grades[name] = "Outstanding"
+    elif student_scores[name] >= 81 and student_scores[name] <= 90:
+        student_grades[name] = "Exceeds Expectations"
+    elif student_scores[name] >= 71 and student_scores[name] <= 80:
+        student_grades[name] = "Acceptable"
+    elif student_scores[name] <= 70:
+        student_grades[name] = "Fail"
+
+print(student_grades)
