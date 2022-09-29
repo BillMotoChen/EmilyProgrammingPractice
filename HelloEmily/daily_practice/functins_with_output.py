b126b4a766231391c66e4919af8571f92d79e290
matsuyama2000 <emily20000222@keio.jp>
2022-09-29 14:35:54 +0900

    learning functions with outputs

---
+++ HelloEmily/daily_practice/functins_with_output.py
@@ -0,0 +1,19 @@
+# functions
+def my_fuction():
+    # do this
+    # then do this
+    # finally do ths
+
+# function with input
+def my_function(something):
+    # do this with something
+    # then do this
+    # finally do this
+
+# function with output
+def my_function():
+    result = 3 * 2
+    return result
+
+output = my_function()
+
